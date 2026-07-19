63ee4eda89c6754f49375689091c492b059e3532
Namdevsingh <namdev.singh_cs23@gla.ac.in>
2026-07-19 16:37:34 +0530

    adding new folder

---
+++ practice_1/KilometersToMilesInput.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class KilometersToMilesInput {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double km = input.nextInt();
+        double miles = km * 1.6;
+        System.out.print("The total miles is " + miles + " mile for the given " + km + " km");
+    }
+}
+
---
+++ practice_1/SamPCM.java
@@ -0,0 +1,11 @@
+public class SamPCM {
+    public static void main(String[] args) {
+        double maths = 94;
+        double physics = 95;
+        double chemistry = 96;
+        double totalPercent = maths + physics + chemistry;
+        double average = totalPercent / 3.0;
+        System.out.print("Sam’s average mark in PCM is " + average);
+    }
+}
+
